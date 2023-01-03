6092ca3b1bcc1cfa657e15e252f2aef2c2802ee9
Piotr Lasota <piotr.lasota@shijigroup.com>
2023-01-03 19:14:01 +0100

    chore: packages bump, styling alignment

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import {Box, Container, Typography} from "@mui/material";
-import SEO from "../components/SeoHelmet";
+import { Box, Container, Typography } from '@mui/material';
+import SEO from '../components/SeoHelmet';
 
-const NotFound = () => (
-  <Container maxWidth="xs">
-    <SEO title="Strona nie istnieje" description="Strona nie istnieje"/>
-    <Box mt={4}>
-      <Typography variant="h4" component="h1" gutterBottom align="center">
-        Ta strona nie istnieje
-      </Typography>
-    </Box>
-  </Container>
-);
+function NotFound() {
+  return (
+    <Container maxWidth="xs">
+      <SEO title="Strona nie istnieje" description="Strona nie istnieje" />
+      <Box mt={4}>
+        <Typography variant="h4" component="h1" gutterBottom align="center">
+          Ta strona nie istnieje
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
 
 export default NotFound;
