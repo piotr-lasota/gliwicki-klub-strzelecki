145559e1eb25c37e10501e00eb72ce227c20e1eb
Piotr Lasota <lasota.piotr@gmail.com>
2024-05-06 16:07:26 +0200

    feat: take down sign-up information

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,6 @@ import SEO from '../components/SeoHelmet';
 import LocationCard from '../components/cards/LocationCard';
 import OpeningHoursCard from '../components/cards/OpeningHoursCard';
 import MembershipCard from '../components/cards/MembershipCard';
-import SignUpCard from '../components/cards/SignUpCard';
 
 export default function Index() {
   return (
@@ -17,9 +16,6 @@ export default function Index() {
         <Grid container direction="row" spacing={4} alignItems="center">
           <Grid item xs={12}>
             <StaticImage src="../images/logo-gks-black.png" alt="Logo klubu" />
-          </Grid>
-          <Grid item xs={12}>
-            <SignUpCard />
           </Grid>
           <Grid item xs={12}>
             <MembershipCard />
