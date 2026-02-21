410bdc7325f9988409fb3b5b59a2bf40c1befd40
Piotr Lasota <lasota.piotr@gmail.com>
2026-02-21 23:33:14 +0100

    child protection update

--- src/components/cards/ChildProtectionCard.jsx
+++ src/components/cards/ChildProtectionCard.jsx
@@ -5,12 +5,12 @@ import { CardActionArea } from 'gatsby-theme-material-ui';
 function ChildProtectionCard() {
   return (
     <Card>
-      <CardActionArea to="https://gliwickiklubstrzelecki.blob.core.windows.net/documents/2025.09.01%20Standard%20Ochrony%20Ma%C5%82oletnich.pdf">
+      <CardActionArea to="https://gliwickiklubstrzelecki.blob.core.windows.net/documents/2026.02.17%20Standard%20Ochrony%20Ma%C5%82oletnich.pdf">
         <CardHeader title="Standardy ochrony małoletnich" />
         <CardContent>
           <Typography>
             Kliknij tutaj aby pobrać obowiązujące standardy ochrony małoletnich
-            z dn. 01.09.2025
+            z dn. 17.02.2026
           </Typography>
         </CardContent>
       </CardActionArea>
